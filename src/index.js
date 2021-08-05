bea5e7cdb4450a327811af9df03d93f9b62e0edd
Marcin Wosinek <marcin@wosinek.eu>
2021-08-05 07:52:43 +0200

    add more method calls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import dayjs from "dayjs";
 
+const today = dayjs(),
+  quarterStart = dayjs().startOf("quarter"),
+  diffDays = today.diff(quarterStart, "days");
+
 console.log("Yesterday was", dayjs().subtract(1, "day").toDate());
+
+console.log(
+  "The quarter started on",
+  quarterStart.format("YYYY-MM-DD"),
+  "and today, it is",
+  diffDays,
+  "days since then"
+);
